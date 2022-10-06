24a7db9a311b73a046877dce10f6aa4ad406683d
Maheshbots <113753302+Maheshbots@users.noreply.github.com>
2022-10-06 11:03:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -689,7 +689,7 @@ async def auto_filter(client, msg, spoll=False):
     btn.insert(0, [
         InlineKeybordButton('🙏 𝙹𝙾𝙸𝙽 𝙼𝚈 𝚄𝙿𝙳𝙰𝚃𝙴𝚂 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 🙏', url='https://t.me/Ms_458')
     ])
-    try:
+
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
     TEMPLATE = settings['template']
     if imdb:
