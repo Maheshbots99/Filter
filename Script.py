0aa94c4aea2e029404fd729480b9be3e9a3e6ea0
Maheshbots <113753302+Maheshbots@users.noreply.github.com>
2022-10-06 19:33:31 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,86 +11,86 @@ class script(object):
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: <a href=https://t.me/Ms_458>𝙲𝙻𝙸𝙲𝙺 𝙷𝙴𝚁𝙴</a>
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>𝗡𝗢𝗧𝗘:</b>
-★ 𝙼𝙰𝙷𝙴𝚂𝙷.𝚂 : <a href=https://t.me/MaHi_458>𝙲𝙻𝙸𝙲𝙺 𝙷𝙴𝚁𝙴</a>
-★ 𝙼𝚂.𝚄𝙿𝙳𝙰𝚃𝙴𝚂: <a href=https://t.me/Ms_458>𝙼𝚂.𝚄𝙿𝙳𝙰𝚃𝙴𝚂</a>
+<b>★ 𝙼𝙰𝙷𝙴𝚂𝙷.𝚂 : <a href=https://t.me/MaHi_458>𝙲𝙻𝙸𝙲𝙺 𝙷𝙴𝚁𝙴</a>
+<b>★ 𝙼𝚂.𝚄𝙿𝙳𝙰𝚃𝙴𝚂: <a href=https://t.me/Ms_458>𝙼𝚂.𝚄𝙿𝙳𝙰𝚃𝙴𝚂</a>
 
-<b>DEVS:</b>
-★ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/MaHi_458>𝙲𝙻𝙸𝙲𝙺 𝙷𝙴𝚁𝙴</a>"""
+<b>𝗢𝗪𝗡𝗘𝗥:</b>
+<b>★ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/MaHi_458>𝙲𝙻𝙸𝙲𝙺 𝙷𝙴𝚁𝙴</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
-- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
+<b>★ Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message</b>
 
 <b>𝗡𝗢𝗧𝗘:</b>
-1. 𝙼𝙰𝙷𝙴𝚂𝙷.𝚂 should have admin privillage.
-2. only admins can add filters in a chat.
-3. alert buttons have a limit of 64 characters.
+<b>1. 𝙼𝙰𝙷𝙴𝚂𝙷.𝚂 should have admin privillage.</b>
+<b>2. only admins can add filters in a chat.</b>
+<b>3. alert buttons have a limit of 64 characters.</b>
 
-<b>Commands and Usage:</b>
-• /filter - <code>add a filter in chat</code>
-• /filters - <code>list all the filters of a chat</code>
-• /del - <code>delete a specific filter in chat</code>
-• /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
+<b>𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗔𝗡𝗗 𝗨𝗦𝗔𝗚𝗘:</b>
+<b>• /filter - <code>add a filter in chat</b></code>
+<b>• /filters - <code>list all the filters of a chat</b></code>
+<b>• /del - <code>delete a specific filter in chat</b></code>
+<b>• /delall - <code>delete the whole filters in a chat (chat owner only)</b></code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- 𝙼𝙰𝙷𝙴𝚂𝙷.𝚂 Supports both url and alert inline buttons.
+<b>⍟ 𝙼𝙰𝙷𝙴𝚂𝙷.𝚂 Supports both url and alert inline buttons.</b>
 
 <b>𝗡𝗢𝗧𝗘:</b>
-1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. 𝙼𝙰𝙷𝙴𝚂𝙷.𝚂 supports buttons with any telegram media type.
-3. Buttons should be properly parsed as markdown format
+<b>1. Telegram will not allows you to send buttons without any content, so content is mandatory.</b>
+<b>2. 𝙼𝙰𝙷𝙴𝚂𝙷.𝚂 supports buttons with any telegram media type.</b>
+<b>3. Buttons should be properly parsed as markdown format</b>
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/Ms_458)</code>
+<code><b>[Button Text](buttonurl:https://t.me/Ms_458)</b></code>
 
 <b>Alert buttons:</b>
-<code>[Button Text](buttonalert:This is an alert message)</code>"""
+<code><b>[Button Text](buttonalert:This is an alert message)</b></code>"""
     AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
 
 <b>𝗡𝗢𝗧𝗘:</b>
-1. Make me the admin of your channel if it's private.
-2. make sure that your channel does not contains camrips, porn and fake files.
-3. Forward the last message to me with quotes.
- I'll add all the files in that channel to my db."""
+<b>1. Make me the admin of your channel if it's private.</b>
+<b>2. make sure that your channel does not contains camrips, porn and fake files.</b>
+<b>3. Forward the last message to me with quotes.</b>
+ <b>I'll add all the files in that channel to my db.</b>"""
     CONNECTION_TXT = """Help: <b>Connections</b>
 
-- Used to connect bot to PM for managing filters
-- it helps to avoid spamming in groups.
+<b>⍟ Used to connect bot to PM for managing filters</b>
+<b>⍟ it helps to avoid spamming in groups.</b>
 
 <b>𝗡𝗢𝗧𝗘:</b>
-1. Only admins can add a connection.
-2. Send <code>/connect</code> for connecting me to ur PM
+<b>1. Only admins can add a connection.</b>
+<b>2. Send <code>/connect</code> for connecting me to ur PM</b>
 
-<b>Commands and Usage:</b>
-• /connect  - <code>connect a particular chat to your PM</code>
-• /disconnect  - <code>disconnect from a chat</code>
-• /connections - <code>list all your connections</code>"""
+<b>𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗔𝗡𝗗 𝗨𝗦𝗔𝗚𝗘:</b>
+<b>• /connect  - <code>connect a particular chat to your PM</b></code>
+<b>• /disconnect  - <code>disconnect from a chat</b></code>
+<b>• /connections - <code>list all your connections</b></code>"""
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
 <b>𝗡𝗢𝗧𝗘:</b>
-these are the extra features of 𝙼𝙰𝙷𝙴𝚂𝙷.𝚂
+<b>these are the extra features of 𝙼𝙰𝙷𝙴𝚂𝙷.𝚂</b>
 
-<b>Commands and Usage:</b>
-• /id - <code>get id of a specified user.</code>
-• /info  - <code>get information about a user.</code>
-• /imdb  - <code>get the film information from IMDb source.</code>
-• /search  - <code>get the film information from various sources.</code>"""
+<b>𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗔𝗡𝗗 𝗨𝗦𝗔𝗚𝗘:</b>
+<b>• /id - <code>get id of a specified user.</b></code>
+<b>• /info  - <code>get information about a user.</b></code>
+<b>• /imdb  - <code>get the film information from IMDb source.</b></code>
+<b>• /search  - <code>get the film information from various sources.</b></code>"""
     ADMIN_TXT = """Help: <b>Admin mods</b>
 
 <b>𝗡𝗢𝗧𝗘:</b>
-This module only works for my admins
+<b>This module only works for my admins</b>
 
-<b>Commands and Usage:</b>
-• /logs - <code>to get the rescent errors</code>
-• /stats - <code>to get status of files in db.</code>
-• /delete - <code>to delete a specific file from db.</code>
-• /users - <code>to get list of my users and ids.</code>
-• /chats - <code>to get list of the my chats and ids</code>
-• /leave  - <code>to leave from a chat.</code>
-• /disable  -  <code>do disable a chat.</code>
-• /ban  - <code>to ban a user.</code>
-• /unban  - <code>to unban a user.</code>
-• /channel - <code>to get list of total connected channels</code>
-• /broadcast - <code>to broadcast a message to all users</code>"""
+<b>𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗔𝗡𝗗 𝗨𝗦𝗔𝗚𝗘:</b>
+<b>• /logs - <code>to get the rescent errors</b></code>
+<b>• /stats - <code>to get status of files in db.</b></code>
+<b>• /delete - <code>to delete a specific file from db.</b></code>
+<b>• /users - <code>to get list of my users and ids.</b></code>
+<b>• /chats - <code>to get list of the my chats and ids</b></code>
+<b>• /leave  - <code>to leave from a chat.</b></code>
+<b>• /disable  -  <code>do disable a chat.</b></code>
+<b>• /ban  - <code>to ban a user.</b></code>
+<b>• /unban  - <code>to unban a user.</b></code>
+<b>• /channel - <code>to get list of total connected channels</b></code>
+<b>• /broadcast - <code>to broadcast a message to all users</b></code>"""
     STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
 ★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
 ★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
