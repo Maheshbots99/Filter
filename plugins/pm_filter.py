e06e78f64274d8635f0a78e3edcb22ab18c013b7
Maheshbots <113753302+Maheshbots@users.noreply.github.com>
2022-10-06 13:38:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -146,7 +146,7 @@ async def advantage_spoll_choker(bot, query):
                                       
                                       ]]
                 ),
-                parse_mode=enums.ParseMode.HTML'
+                parse_mode='HTML
 )
 
             await asyncio.sleep(10)
